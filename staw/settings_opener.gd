d750d1f2c3ee699f32dab824520266213993da8b
H0pelessCoder <158211322+H0pelessCoder@users.noreply.github.com>
2026-02-10 14:29:35 -0800

    Began on news screen

--- staw/settings_opener.gd
+++ staw/settings_opener.gd
@@ -6,6 +6,6 @@ var Settings = $"../../Settings"
 func _on_pressed():
 	self.get_parent().visible = false
 	Settings.visible = true
-	Settings.set_meta("LastOpen", "./MainMenu")
+	Settings.set_meta("LastOpen", "./" + get_parent().name)
 	
 	
